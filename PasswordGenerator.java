5cc311c04378a1f9009a61aadc305b1424483b95
Shafat Alam <Shafat.mahtab@gmail.com>
2022-11-18 12:58:03 +0600

    Commit

---
+++ PasswordGenerator.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class PasswordGenerator
+{
+	private static char[] Passwordgenerator(int lenth) {
+		String Capital_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		String Small_chars = "abcdefghijklmnopqrstuvwxyz";
+		String numbers = "0123456789";
+		String symbols = "!@#$%^&*_=+-/.?<>)";
+
+		String values = Capital_chars + Small_chars +
+						numbers + symbols;
+
+		Random rndm_method = new Random();
+
+		char[] password = new char[lenth];
+
+		for (int i = 0; i < lenth; i++)
+		{
+
+			password[i] =
+			values.charAt(rndm_method.nextInt(values.length()));
+
+		}
+        return password;
+	}
+	public static void main(String[] args) 
+	{
+	    try (Scanner len = new Scanner(System.in)) {
+			System.out.println("Enter your password Length");
+
+			int length = len.nextInt(); 
+			System.out.println("Your Password is: " + length); 
+			// int length = 10;
+			System.out.println(Passwordgenerator(length));
+		} 
+	}
+
+}
+
